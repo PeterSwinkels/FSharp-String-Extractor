84b0d6b9f83418a849e736a0c8ed1faa04690db6
Peter Swinkels <peter_swinkels@hotmail.com>
2021-12-26 23:31:28 +0100

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿//This module's imports and settings.
+open System.IO
+
+//This function is executed when this program is started.
+[<EntryPoint>]
+let main argv = 
+   try      
+      let rec CollectFragments characters fragments = 
+         if characters = [] then
+            fragments
+         else
+            CollectFragments characters.Tail (fragments +                                          
+                                          if characters.Head >= ' ' && characters.Head <= '~' then characters.Head |> string
+                                          else if fragments.EndsWith(0uy |> char |> string) then ""
+                                          else 0uy |> char |> string)
+      (CollectFragments (File.ReadAllBytes(argv.[0]) |> Array.map char |> Array.toList) "").Split(0uy |> char) |> Array.distinct |> Array.iter(fun newFragment -> printfn "%s" newFragment)
+   with
+      | exceptionO -> printfn("Error: \"%s\"") exceptionO.Message
+   0
